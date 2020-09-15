c68768bd6b70b7a365398ae2c1a642dfcfccced6
wyao <289672494@qq.com>
2020-09-15 21:50:26 +0800

    [wang yao]实现添加一条热搜

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,17 +1,15 @@
 package com.thoughtworks.rslist.api;
 
 import org.hibernate.annotations.Parameter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-    private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+    private List<String> rsList = new ArrayList<String>(Arrays.asList("第一条事件", "第二条事件", "第三条事件"));
 
     @GetMapping("/rs/event/{index}")
     public String getOneEvent(@PathVariable int index) {
@@ -22,5 +20,15 @@ public class RsController {
     public String getRangeEvents(@RequestParam(required = false, defaultValue = "1") Integer start,
                           @RequestParam(required = false, defaultValue = "2") Integer end) {
       return rsList.subList(start-1, end).toString();
+    }
+
+    @GetMapping("rs/event/list_all")
+    public String getAllEvent() {
+        return rsList.toString();
+    }
+
+    @PostMapping("rs/event")
+    public void addOneEvent(@RequestBody String event) {
+        rsList.add(event);
     }
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -5,14 +5,18 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode =  DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RsListApplicationTests {
 
     @Autowired
@@ -30,6 +34,19 @@ class RsListApplicationTests {
         mockMvc.perform(get("/rs/event?start=1&end=3"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
+    }
+
+    @Test
+    void show_add_one_event() throws Exception {
+        mockMvc.perform(get("/rs/event/list_all"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
+        mockMvc.perform(post("/rs/event").content("第四条事件").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/event/list_all"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件, 第四条事件]"));
+
     }
 
 }
