3eefb3f1c06015cb182b0d5a331cad45ad9e8adc
wyao <289672494@qq.com>
2020-09-15 21:05:48 +0800

    [wang yao]实现获取一件热搜事件

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,29 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void show_get_one_event() throws Exception {
+        mockMvc.perform(get("/rs/event/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("第一条事件"));
     }
+
 
 }
