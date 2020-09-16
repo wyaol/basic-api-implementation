629bf68c50f7ba8b6137984a98975bde191e85b5
wyao <289672494@qq.com>
2020-09-17 00:06:09 +0800

    [wang yao]添加日志打印

--- src/main/java/com/thoughtworks/rslist/api/GlobalController.java
+++ src/main/java/com/thoughtworks/rslist/api/GlobalController.java
@@ -2,12 +2,16 @@ package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.dto.ErrorDto;
 import com.thoughtworks.rslist.exceptions.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalController {
+
+    Logger logger = LoggerFactory.getLogger(GlobalController.class);
 
     @ExceptionHandler({
             InvalidRequestParamException.class,
@@ -16,6 +20,7 @@ public class GlobalController {
             InvalidUserException.class
     })
     public ResponseEntity handleException(Exception e) {
+        logger.error(e.getMessage());
         return ResponseEntity.status(400).body(new ErrorDto(e.getMessage()));
     }
 }
