3e90d6622f90112d9dc1833f9df6ada366a5e141
wyao <289672494@qq.com>
2020-09-16 16:47:21 +0800

    [wang yao]更新RsController Response

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.dto.Event;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,11 +11,6 @@ import java.util.List;
 @RestController
 public class RsController {
 
-    @GetMapping("/rs/list")
-    public String getList() {
-        return rsList.toString();
-    }
-
     private List<Event> rsList = new ArrayList<Event>(Arrays.asList(
             new Event("第一条事件", "其他"),
             new Event("第二条事件", "其他"),
@@ -22,34 +18,37 @@ public class RsController {
     ));
 
     @GetMapping("/rs/event/{index}")
-    public Event getOneEvent(@PathVariable int index) {
-        return rsList.get(index - 1);
+    public ResponseEntity getOneEvent(@PathVariable int index) {
+        return ResponseEntity.status(201).body(rsList.get(index - 1));
     }
 
     @GetMapping("rs/event")
-    public List<Event> getRangeEvents(@RequestParam(required = false, defaultValue = "1") Integer start,
+    public ResponseEntity getRangeEvents(@RequestParam(required = false, defaultValue = "1") Integer start,
                                       @RequestParam(required = false, defaultValue = "2") Integer end) {
-        return rsList.subList(start - 1, end);
+        return ResponseEntity.status(201).body(rsList.subList(start - 1, end));
     }
 
     @GetMapping("rs/event/list_all")
-    public List<Event> getAllEvent() {
-        return rsList;
+    public ResponseEntity getAllEvent() {
+        return ResponseEntity.status(201).body(rsList);
     }
 
     @PostMapping("rs/event")
-    public void addOneEvent(@RequestBody Event event) {
+    public ResponseEntity addOneEvent(@RequestBody Event event) {
         rsList.add(event);
+        return ResponseEntity.status(201).build();
     }
 
     @PutMapping("rs/event/{index}")
-    public void editOneEvent(@PathVariable("index") int index, @RequestBody Event event) {
+    public ResponseEntity editOneEvent(@PathVariable("index") int index, @RequestBody Event event) {
         if (event.getEventName() != null) rsList.get(index - 1).setEventName(event.getEventName());
         if (event.getKeyWord() != null) rsList.get(index - 1).setKeyWord(event.getKeyWord());
+        return ResponseEntity.status(201).build();
     }
 
     @DeleteMapping("rs/event/{index}")
-    public void deleteOneEvent(@PathVariable("index") int index) {
+    public ResponseEntity deleteOneEvent(@PathVariable("index") int index) {
         if (index < rsList.size()) rsList.remove(index - 1);
+        return ResponseEntity.status(201).build();
     }
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -26,14 +26,14 @@ class RsListApplicationTests {
     @Test
     void show_get_one_event() throws Exception {
         mockMvc.perform(get("/rs/event/1"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andExpect(jsonPath("$.eventName", is("第一条事件")));
     }
 
     @Test
     void show_get_range_event() throws Exception {
         mockMvc.perform(get("/rs/event?start=1&end=3"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
                 .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
@@ -43,7 +43,7 @@ class RsListApplicationTests {
     @Test
     void show_add_one_event() throws Exception {
         mockMvc.perform(get("/rs/event/list_all"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
                 .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
@@ -52,9 +52,9 @@ class RsListApplicationTests {
         ObjectMapper objectMapper = new ObjectMapper();
 
         mockMvc.perform(post("/rs/event").content(objectMapper.writeValueAsString(new Event("第四条事件", "无分类"))).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().is(201));
         mockMvc.perform(get("/rs/event/list_all"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andExpect(jsonPath("$", hasSize(4)))
                 .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
                 .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
@@ -66,29 +66,29 @@ class RsListApplicationTests {
     @Test
     void should_edit_one_event() throws Exception {
         mockMvc.perform(get("/rs/event/1"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andExpect(jsonPath("$.eventName", is("第一条事件")));
         mockMvc.perform(put("/rs/event/1").content(new ObjectMapper().writeValueAsString(new Event("第一件修改后的事件", null))).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().is(201));
         mockMvc.perform(get("/rs/event/1"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andExpect(jsonPath("$.eventName", is("第一件修改后的事件")));
     }
 
     @Test
     void should_delete_one_event() throws Exception {
         mockMvc.perform(get("/rs/event/list_all"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
                 .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
                 .andExpect(jsonPath("$[2].eventName", is("第三条事件")));
 
         mockMvc.perform(delete("/rs/event/2"))
-                .andExpect(status().isOk());
+                .andExpect(status().is(201));
 
         mockMvc.perform(get("/rs/event/list_all"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
                 .andExpect(jsonPath("$[1].eventName", is("第三条事件")));
